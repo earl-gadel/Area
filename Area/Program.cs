7801165ecbb46037e8524b131a7ee5e235b6d6f9
Earl <earljg@gmail.com>
2018-05-06 18:28:36 -0500

    renamed variables

--- Area/Program.cs
+++ Area/Program.cs
@@ -7,22 +7,22 @@ namespace Area
         static void Main(string[] args)
         {
             string input;
-            double r;
-            double a;
+            double radius;
+            double area;
 
             Console.WriteLine("Enter a radius: ");
             input = Console.ReadLine();
-            r = double.Parse(input);
+            radius = double.Parse(input);
             do
             {
                 string message = "Enter a positive number.";
                 Console.WriteLine(message);
                 Console.WriteLine("Enter a radius: ");
                 input = Console.ReadLine();
-                r = double.Parse(input);
-            } while (r < 0);
-            a = 3.14159 * r * r;
-            Console.WriteLine("The area of a circle with radius {0} is: {1}", r, a);
+                radius = double.Parse(input);
+            } while (radius < 0);
+            area = 3.14159 * radius * radius;
+            Console.WriteLine("The area of a circle with radius {0} is: {1}", radius, area);
             Console.ReadLine();
         }
     }
